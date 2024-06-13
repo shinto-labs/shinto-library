1f448a74ea191187cbb34b25666892f85a6f6b97
Aron Hemmes <aron@shintolabs.nl>
2024-06-13 11:04:53 +0200

    Update uvicorn config method

--- shinto/__init__.py
+++ shinto/__init__.py
@@ -1,5 +1,5 @@
 """
 Shinto Library
 """
-from .logging import setup_logging, setup_uvicorn_log_config
+from .logging import setup_logging, generate_uvicorn_log_config
 from .config import load_config_file, output_config
