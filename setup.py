a1af1eeb3cc328be4ef716c954ab369292cc32dc
Aron Hemmes <aron@shintolabs.nl>
2024-07-05 12:50:38 +0200

    update deploy script

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='shinto',
-    version='1.0.0',
+    version='1.0.1',
     description='Shinto Labs default python library',
     author='Tommy van Schie',
     author_email='tommy@shintolabs.nl',
