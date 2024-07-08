b1e3ba6375262b997bb099149767a64198d8db7b
Aron Hemmes <aron@shintolabs.nl>
2024-07-08 20:19:09 +0200

    Add database connection class and format code with ruff

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python3
+"""Setup file for the shinto package."""
 
 from setuptools import setup
 
 setup(
-    name='shinto',
-    version='1.0.2',
-    description='Shinto Labs default python library',
-    author='Tommy van Schie',
-    author_email='tommy@shintolabs.nl',
-    url='http://www.shintolabs.nl',
-    packages=[
-        'shinto'
-    ],
-    install_requires=[
-        'pyyaml',
-    ]
+    name="shinto",
+    version="1.0.3",
+    description="Shinto Labs default python library",
+    author="Tommy van Schie",
+    author_email="tommy@shintolabs.nl",
+    url="http://www.shintolabs.nl",
+    packages=["shinto"],
+    install_requires=["pyyaml", "psycopg[pool]", "jsonschema"],
 )
--- shinto/__init__.py
+++ shinto/__init__.py
@@ -1,5 +1,14 @@
-"""
-Shinto Library
-"""
-from .logging import setup_logging, generate_uvicorn_log_config
+"""Shinto Library."""
+
+__all__ = [
+    "setup_logging",
+    "generate_uvicorn_log_config",
+    "load_config_file",
+    "output_config",
+    "DatabaseConnection",
+    "database_connection_from_config",
+]
+
 from .config import load_config_file, output_config
+from .database_connection import DatabaseConnection, database_connection_from_config
+from .logging import generate_uvicorn_log_config, setup_logging
