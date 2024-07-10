b71530c0f04193d3567e49f7f7f6960fb34635b7
Aron Hemmes <aron@shintolabs.nl>
2024-07-10 14:43:34 +0200

    Update logging

--- tests/test_logging.py
+++ tests/test_logging.py
@@ -3,7 +3,7 @@
 import logging
 import unittest
 
-from shinto.logging import generate_uvicorn_log_config, setup_logging
+from shinto.logging import setup_logging
 
 
 class TestLogging(unittest.TestCase):
@@ -29,23 +29,6 @@ class TestLogging(unittest.TestCase):
         self.assertEqual(logger.level, logging.DEBUG)
         self.assertTrue(isinstance(logger.handlers[0], logging.FileHandler))
         self.assertEqual(len(logger.handlers), 1)
-
-    def test_generate_uvicorn_log_config(self):
-        """Test generate_uvicorn_log_config function with default parameters."""
-        config = generate_uvicorn_log_config()
-        self.assertEqual(config["version"], 1)
-        self.assertIsInstance(config["loggers"], dict)
-
-        # Test generate_uvicorn_log_config function with custom parameters
-        config = generate_uvicorn_log_config(
-            loglevel=logging.DEBUG,
-            log_to_stdout=False,
-            log_to_file=True,
-            log_to_journald=True,
-            log_filename="app.log",
-        )
-        self.assertEqual(config["version"], 1)
-        self.assertIsInstance(config["loggers"], dict)
 
 
 if __name__ == "__main__":
