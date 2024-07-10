460d456b313b6f601fbca7c57c7f3424fef33404
Aron Hemmes <aron@shintolabs.nl>
2024-07-10 17:18:44 +0200

    Update uvicorn tests

--- tests/test_uvicorn_entrypoint.py
+++ tests/test_uvicorn_entrypoint.py
@@ -4,6 +4,7 @@ import ast
 import http.client
 import logging
 import threading
+import time
 import unittest
 
 from fastapi import FastAPI
@@ -40,18 +41,43 @@ def logger():
     }
 
 
+def wait_for_server_to_start(host, port, timeout=5):
+    """Wait for the server to start by attempting to connect within a timeout period."""
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        try:
+            conn = http.client.HTTPConnection(host, port)
+            conn.request("HEAD", "/")
+            conn.close()
+            return True
+        except ConnectionRefusedError:
+            time.sleep(0.2)
+    return False
+
+
 class TestUvicornEntrypoint(unittest.TestCase):
     """Tests for uvicorn entrypoint module."""
 
+    server_thread = None
+    loglevel: int = None
+    port: int = None
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up the FastAPI app server in a separate thread and ensure it's ready."""
+        cls.loglevel = logging.INFO
+        cls.port = 40010
+        logging.root.setLevel(cls.loglevel)
+        cls.server_thread = threading.Thread(
+            target=run_fastapi_app, args=(app, "0.0.0.0", cls.port, False), daemon=True
+        )
+        cls.server_thread.start()
+        if not wait_for_server_to_start("localhost", cls.port):
+            raise Exception("Failed to start the FastAPI server.")
+
     def test_run_fastapi_app(self):
         """Test that the FastAPI app can be run."""
-        server_thread = threading.Thread(
-            target=run_fastapi_app, args=(app, "0.0.0.0", 40010, False), daemon=True
-        )
-        server_thread.start()
-        self.assertTrue(server_thread.is_alive())
-
-        conn = http.client.HTTPConnection("localhost", 40010)
+        conn = http.client.HTTPConnection("localhost", self.port)
         conn.request("GET", "/")
         response = conn.getresponse()
         response_data = ast.literal_eval(response.read().decode())
@@ -60,15 +86,7 @@ class TestUvicornEntrypoint(unittest.TestCase):
 
     def test_logger_propagation(self):
         """Test that the loggers are set correctly."""
-        loglevel = logging.INFO
-        logging.root.setLevel(loglevel)
-        server_thread = threading.Thread(
-            target=run_fastapi_app, args=(app, "0.0.0.0", 40011, False), daemon=True
-        )
-        server_thread.start()
-        self.assertTrue(server_thread.is_alive())
-
-        conn = http.client.HTTPConnection("localhost", 40011)
+        conn = http.client.HTTPConnection("localhost", self.port)
         conn.request("GET", "/loggers")
         response = conn.getresponse()
         response_data = ast.literal_eval(response.read().decode())
@@ -76,7 +94,7 @@ class TestUvicornEntrypoint(unittest.TestCase):
         for logger in response_data["loggers"]:
             if logger["name"] == "root":
                 continue
-            self.assertEqual(logger["level"], logging.getLevelName(loglevel))
+            self.assertEqual(logger["level"], logging.getLevelName(self.loglevel))
             self.assertEqual(logger["propagate"], "True")
             self.assertListEqual(logger["handlers"], [])
 
