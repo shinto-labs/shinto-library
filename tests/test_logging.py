58febebdb1c972efa3940c1ab297bf562c327af5
Tommy van Schie <tommy@shintolabs.nl>
2024-07-10 11:53:35 +0200

    Added some tests

---
+++ tests/test_logging.py
@@ -0,0 +1,36 @@
+import logging
+import unittest
+from shinto.logging import setup_logging, generate_uvicorn_log_config
+
+# FILEPATH: /home/tommy/work/shintolabs/shinto-library/tests/test_logging.py
+
+
+class TestLogging(unittest.TestCase):
+    def test_setup_logging(self):
+        # Test setup_logging function with default parameters
+        setup_logging()
+        logger = logging.getLogger()
+        self.assertEqual(logger.level, logging.WARNING)
+        self.assertTrue(isinstance(logger.handlers[0], logging.StreamHandler))
+        self.assertEqual(len(logger.handlers), 1)
+
+        # Test setup_logging function with custom parameters
+        setup_logging(application_name="myapp", loglevel=logging.DEBUG, log_to_stdout=False, log_to_file=True, log_filename="app.log")
+        logger = logging.getLogger("myapp")
+        self.assertEqual(logger.level, logging.DEBUG)
+        self.assertTrue(isinstance(logger.handlers[0], logging.FileHandler))
+        self.assertEqual(len(logger.handlers), 1)
+
+    def test_generate_uvicorn_log_config(self):
+        # Test generate_uvicorn_log_config function with default parameters
+        config = generate_uvicorn_log_config()
+        self.assertEqual(config["version"], 1)
+        self.assertIsInstance(config["loggers"], dict)
+
+        # Test generate_uvicorn_log_config function with custom parameters
+        config = generate_uvicorn_log_config(loglevel=logging.DEBUG, log_to_stdout=False, log_to_file=True, log_to_journald=True, log_filename="app.log")
+        self.assertEqual(config["version"], 1)
+        self.assertIsInstance(config["loggers"], dict)
+
+if __name__ == "__main__":
+    unittest.main()
