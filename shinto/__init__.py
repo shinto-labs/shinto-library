ff8261d2a3bd2b13e86f4b26c5e8cdde6de36ab9
Tommy van Schie <tommy@shintolabs.nl>
2024-07-11 10:03:24 +0200

    Added push_metrics function

--- shinto/__init__.py
+++ shinto/__init__.py
@@ -1,6 +1,12 @@
 """Shinto Library."""
 
-__all__ = ["setup_logging", "generate_uvicorn_log_config", "load_config_file", "output_config"]
+__all__ = [
+    "setup_logging",
+    "generate_uvicorn_log_config",
+    "load_config_file",
+    "output_config",
+    "push_metric"]
 
 from .config import load_config_file, output_config
 from .logging import setup_logging
+from .metrics import push_metric
