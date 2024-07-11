ca723a4c486ba0e4851a886f5d6b2ab4fdb78998
Aron Hemmes <aron@shintolabs.nl>
2024-07-11 14:20:18 +0200

    Clean up logging

--- shinto/logging.py
+++ shinto/logging.py
@@ -3,7 +3,6 @@
 import logging
 import sys
 from importlib.util import find_spec
-from typing import Union
 
 SHINTO_LOG_FORMAT = (
     "%(asctime)s.%(msecs)03d - [%(process)06d] %(name)s - %(levelname)s - %(message)s"
@@ -12,12 +11,12 @@ SHINTO_LOG_DATEFMT = "%Y-%m-%d %H:%M:%S"
 
 
 def setup_logging(
-    application_name: str = None,
-    loglevel: Union[str, int] = logging.WARNING,
+    application_name: str | None = None,
+    loglevel: str | int = logging.WARNING,
     log_to_stdout: bool = True,
     log_to_file: bool = True,
     log_to_journald: bool = False,
-    log_filename: str = None,
+    log_filename: str | None = None,
 ):
     """Set up logging, format etc."""
     if not application_name:
@@ -51,12 +50,12 @@ def setup_logging(
         try:
             if find_spec("JournalHandler") is None:
                 # systemd is not available on all platforms (including Windows)
-                from systemd.journal import JournalHandler  # type: ignore # noqa: F401 I001
+                from systemd.journal import JournalHandler  # type: ignore[reportMissingImports]
 
             journald_handler = JournalHandler()
             journald_handler.setFormatter(formatter)
             logger.addHandler(journald_handler)
         except ImportError:
-            logger.error("Failed to import systemd.journal. Journal logging not available.")
+            logger.exception("Failed to import systemd.journal. Journal logging not available.")
 
     logging.root = logger
