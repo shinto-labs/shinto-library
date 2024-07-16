7da61b4c2943c8e8d055be79072e8f11b7bfaff8
Aron Hemmes <aron@shintolabs.nl>
2024-07-16 15:24:29 +0200

    Update scripts

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -5,12 +5,6 @@ set -e
 # find all cache files and remove them
 find . -name "__pycache__" | xargs rm -rf
 
-# find egg-info directories and remove them
-find . -name "*.egg-info" | xargs rm -rf
-
-# find build directories and remove them
-find . -name "build" | xargs rm -rf
-
 # find coverage files and remove them
 find . -name ".coverage" | xargs rm -rf
 find . -name "*,cover" | xargs rm -rf
@@ -19,4 +13,5 @@ find . -name "htmlcov" | xargs rm -rf
 # clean up pipenv packages
 pipenv clean
 
-echo "Cleaned up the project!"
+echo "
+Cleaned up the project!"
