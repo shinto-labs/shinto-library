4b1b739ed068b5587e4ea60ec6c3bf906d077cb1
Aron Hemmes <aron@shintolabs.nl>
2024-07-17 16:29:59 +0200

    Remove pipenv shell, as this can't be done inside bash scripts and update script message

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,15 +6,7 @@ yellow='\033[1;93m'
 green='\033[0;32m'
 reset='\033[0m'
 
-# if [[ "$VIRTUAL_ENV" == "" ]]; then
-#     pip install pipenv
-# fi
-
 pipenv install --dev
-
-if [[ "$VIRTUAL_ENV" == "" ]]; then
-    pipenv shell
-fi
 
 venv_path=$(pipenv --venv)
 
@@ -27,7 +19,15 @@ Installation complete.
 In Visual Studio Code, set the venv as your Python interpreter:
 1. Open the Command Palette ${green}Ctrl+Shift+P${reset}
 2. Type ${green}Python: Select Interpreter${reset}
-3. Select the venv: ${green}${venv_path}\\Scripts\\python.exe${reset}
+3. Select the venv: ${green}${venv_path}\\Scripts\\python.exe${reset}"
 
+if [ -z "$VIRTUAL_ENV" ]; then
+    echo -e "
 To activate this project's virtualenv in shell, run: ${yellow}pipenv shell${reset}
+Alternatively, run a command inside the virtualenv with ${yellow}pipenv run <command>${reset}
 "
+else
+    echo -e "
+${green}Virtualenv is already activated in shell.${reset}
+"
+fi
