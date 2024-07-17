3ba57a3d10b65ccd9bd96d15670c2b9e0cb7e2cc
Aron Hemmes <aron@shintolabs.nl>
2024-07-17 16:37:13 +0200

    Add clarification comment  to install bash script

--- scripts/install.sh
+++ scripts/install.sh
@@ -21,6 +21,7 @@ In Visual Studio Code, set the venv as your Python interpreter:
 2. Type ${green}Python: Select Interpreter${reset}
 3. Select the venv: ${green}${venv_path}\\Scripts\\python.exe${reset}"
 
+# Shell cannot be activated from a bash script, so we can only provide instructions
 if [ -z "$PIPENV_ACTIVE" ]; then
     echo -e "
 To activate this project's virtualenv in shell, run: ${yellow}pipenv shell${reset}
