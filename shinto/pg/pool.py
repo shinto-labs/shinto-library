14704a5739b4d5140ef8d201468d1ef1437a9f3b
Aron Hemmes <aron@shintolabs.nl>
2024-08-23 12:21:35 +0200

    Update tests and clean up code

--- shinto/pg/pool.py
+++ shinto/pg/pool.py
@@ -7,7 +7,7 @@ from typing import TYPE_CHECKING
 
 import psycopg_pool
 
-if TYPE_CHECKING:
+if TYPE_CHECKING:  # pragma: no cover
     from collections.abc import AsyncGenerator, Generator
 
     from shinto.pg.connection import AsyncConnection, Connection
@@ -17,18 +17,7 @@ class ConnectionPool(psycopg_pool.ConnectionPool):
     """ConnectionPool class."""
 
     @contextmanager
-    def connection(self, timeout: float | None = None) -> Generator[Connection, None, None]:
-        """
-        Context manager to obtain a connection from the pool.
-
-        Return the connection immediately if available, otherwise wait up to timeout or self.timeout
-        seconds and throw PoolTimeout if a connection is not available in time.
-
-        Upon context exit, return the connection to the pool. Apply the normal connection context
-        behaviour <with-connection> (commit/rollback the transaction in case of success/error).
-        If the connection is no more in working state, replace it with a new one.
-
-        """
+    def connection(self, timeout: float | None = None) -> Generator[Connection, None, None]:  # noqa: D102, inherit docstring from base method
         with super().connection(timeout) as conn:
             yield conn
 
@@ -37,20 +26,9 @@ class AsyncConnectionPool(psycopg_pool.AsyncConnectionPool):
     """AsyncConnectionPool class."""
 
     @asynccontextmanager
-    async def connection(
+    async def connection(  # noqa: D102, inherit docrstring from base method
         self,
         timeout: float | None = None,  # noqa: ASYNC109, RUF100 does not make sense for overloading connection method
     ) -> AsyncGenerator[AsyncConnection, None, None]:
-        """
-        Async context manager to obtain a connection from the pool.
-
-        Return the connection immediately if available, otherwise wait up to timeout or self.timeout
-        seconds and throw PoolTimeout if a connection is not available in time.
-
-        Upon context exit, return the connection to the pool. Apply the normal connection context
-        behaviour <with-connection> (commit/rollback the transaction in case of success/error).
-        If the connection is no more in working state, replace it with a new one.
-
-        """
         async with super().connection(timeout) as conn:
             yield conn
--- tests/pg/test_pool.py
+++ tests/pg/test_pool.py
@@ -1,6 +1,5 @@
 """Test the ConnectionPool and AsyncConnectionPool classes."""
 
-import os
 import unittest
 from unittest.mock import AsyncMock, MagicMock, patch
 
@@ -8,134 +7,94 @@ import psycopg
 
 from shinto.pg import AsyncConnectionPool, ConnectionPool
 
-test_config = {
-    "database": "test_db",
-    "user": "test_user",
-    "password": "test_password",
-    "host": "test_host",
-    "port": 6432,
-}
-
 
 class TestConnectionPool(unittest.TestCase):
     """Test the ConnectionPool class."""
 
-    def test_pool_creation(self):
-        """Test the creation of a connection pool."""
-        pool = ConnectionPool(**test_config)
-        self.assertIsInstance(pool, ConnectionPool)
-        self.assertTrue(pool.closed)
-
-    def test_creation_without_args_failure(self):
-        """Test the creation of a connection pool without arguments."""
-        with self.assertRaises(TypeError):
-            ConnectionPool()
-
-    @patch.dict(
-        os.environ,
-        {
-            "PGDATABASE": test_config["database"],
-            "PGUSER": test_config["user"],
-            "PGPASSWORD": test_config["password"],
-            "PGHOST": test_config["host"],
-            "PGPORT": str(test_config["port"]),
-        },
-        clear=True,
-    )
-    def test_pool_creation_from_env_vars(self):
-        """Test the creation of a database connection from environment variables."""
-        pool = ConnectionPool()
-        self.assertIsInstance(pool, ConnectionPool)
-        self.assertTrue(pool.closed)
-
+    @patch("shinto.pg.pool.psycopg_pool.ConnectionPool.__init__", return_value=None)
     @patch("shinto.pg.pool.psycopg_pool.ConnectionPool.connection")
-    def test_connection_context_manager(self, mock_super_connection: MagicMock):
+    def test_connection_context_manager(
+        self,
+        mock_super_connection: MagicMock,
+        mock_connection_pool: MagicMock,
+    ):
         """Test the connection context manager."""
         mock_connection = MagicMock()
         mock_context_manager = MagicMock()
         mock_context_manager.__enter__.return_value = mock_connection
         mock_super_connection.return_value = mock_context_manager
 
-        pool = ConnectionPool(**test_config)
+        pool = ConnectionPool()
 
         with pool.connection() as conn:
             self.assertEqual(conn, mock_connection)
 
         mock_super_connection.assert_called_once()
         mock_context_manager.__enter__.assert_called_once()
+        mock_connection_pool.assert_called_once()
 
+    @patch("shinto.pg.pool.psycopg_pool.ConnectionPool.__init__", return_value=None)
     @patch("shinto.pg.pool.psycopg_pool.ConnectionPool.connection")
-    def test_connection_context_manager_exception(self, mock_super_connection: MagicMock):
+    def test_connection_context_manager_exception(
+        self,
+        mock_super_connection: MagicMock,
+        mock_connection_pool: MagicMock,
+    ):
         """Test the connection context manager exception handling."""
         mock_super_connection.side_effect = psycopg.Error
 
-        pool = ConnectionPool(**test_config)
+        pool = ConnectionPool()
 
         with self.assertRaises(psycopg.Error), pool.connection():
             pass
 
         mock_super_connection.assert_called_once()
+        mock_connection_pool.assert_called_once()
 
 
 class TestAsyncConnectionPool(unittest.IsolatedAsyncioTestCase):
     """Test the AsyncConnectionPool class."""
 
-    def test_pool_creation(self):
-        """Test the creation of a connection pool."""
-        pool = AsyncConnectionPool(**test_config)
-        self.assertIsInstance(pool, AsyncConnectionPool)
-        self.assertTrue(pool.closed)
-
-    def test_creation_without_args_failure(self):
-        """Test the creation of a connection pool without arguments."""
-        with self.assertRaises(TypeError):
-            AsyncConnectionPool()
-
-    @patch.dict(
-        os.environ,
-        {
-            "PGDATABASE": test_config["database"],
-            "PGUSER": test_config["user"],
-            "PGPASSWORD": test_config["password"],
-            "PGHOST": test_config["host"],
-            "PGPORT": str(test_config["port"]),
-        },
-        clear=True,
-    )
-    def test_pool_creation_from_env_vars(self):
-        """Test the creation of a connection pool from environment variables."""
-        pool = AsyncConnectionPool()
-        self.assertIsInstance(pool, AsyncConnectionPool)
-        self.assertTrue(pool.closed)
-
+    @patch("shinto.pg.pool.psycopg_pool.AsyncConnectionPool.__init__", return_value=None)
     @patch("shinto.pg.pool.psycopg_pool.AsyncConnectionPool.connection")
-    async def test_connection_context_manager(self, mock_super_connection: AsyncMock):
+    async def test_connection_context_manager(
+        self,
+        mock_super_connection: AsyncMock,
+        mock_connection_pool: MagicMock,
+    ):
         """Test the connection context manager."""
         mock_connection = AsyncMock()
         mock_context_manager = AsyncMock()
         mock_context_manager.__aenter__.return_value = mock_connection
         mock_super_connection.return_value = mock_context_manager
 
-        pool = AsyncConnectionPool(**test_config)
+        pool = AsyncConnectionPool()
 
         async with pool.connection() as conn:
             self.assertEqual(conn, mock_connection)
 
         mock_super_connection.assert_called_once()
         mock_context_manager.__aenter__.assert_called_once()
+        mock_connection_pool.assert_called_once()
 
+    @patch("shinto.pg.pool.psycopg_pool.AsyncConnectionPool.__init__", return_value=None)
     @patch("shinto.pg.pool.psycopg_pool.AsyncConnectionPool.connection")
-    async def test_connection_context_manager_exception(self, mock_super_connection: AsyncMock):
+    async def test_connection_context_manager_exception(
+        self,
+        mock_super_connection: AsyncMock,
+        mock_connection_pool: MagicMock,
+    ):
         """Test the connection context manager exception handling."""
         mock_super_connection.side_effect = psycopg.Error
 
-        pool = AsyncConnectionPool(**test_config)
+        pool = AsyncConnectionPool()
 
         with self.assertRaises(psycopg.Error):
             async with pool.connection():
                 pass
 
         mock_super_connection.assert_called_once()
+        mock_connection_pool.assert_called_once()
 
 
 if __name__ == "__main__":
